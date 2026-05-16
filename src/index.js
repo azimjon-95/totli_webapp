dd93620fff25eafa14f8240c319b738e54994114
azimjon-95 <azimjon995m@gmail.com>
2026-05-17 01:31:01 +0500

    new version

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./global.css";
 import "./index.css";
 import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode><AppRoutes/></React.StrictMode>);
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <AppRoutes />
+        </BrowserRouter>
+    </React.StrictMode>
+)
+// root.render(<React.StrictMode><AppRoutes/></React.StrictMode>);
